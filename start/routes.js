32c21b2e4216a75dc79fccfd363bbf3da506d232
Diego Moreno <diego.moreno@linx.com.br>
2018-12-20 17:50:59 -0200

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -19,5 +19,5 @@ Route.group(() => {
   //   ]))
 }).middleware(['auth'])
 Route.get('/', () => {
-  return { greeting: 'Bem vindo seu otário!!!' }
+  return { greeting: 'Bem vindo André Artuzo!!!' }
 })
